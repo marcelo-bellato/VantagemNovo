033afee2b1d997f9f76c68d2b7278e63be7dc05b
MarceloBellato <69009163+marcelo-bellato@users.noreply.github.com>
2021-08-13 16:56:47 -0300

    Add files via upload

--- VantagemNovo/src/test/java/br/sp/bellato/steps/Hooks.java
+++ VantagemNovo/src/test/java/br/sp/bellato/steps/Hooks.java
@@ -3,12 +3,21 @@ package br.sp.bellato.steps;
 import static br.sp.bellato.core.DriverFactory.getDriver;
 import static br.sp.bellato.core.DriverFactory.killDriver;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class Hooks {
 	
-
+	private WebDriver driver;
 		
 	@Before
 	public void inicializa() {		
@@ -16,7 +25,17 @@ public class Hooks {
 	}
 			
 	
-	@After
+	@After(order = 1)
+	public void screenshot(Scenario cenario) {
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(file, new File("target/screenshot/"+cenario.getId()+".jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@After(order = 0)
 	public void finaliza() {
 		killDriver();
 	}
